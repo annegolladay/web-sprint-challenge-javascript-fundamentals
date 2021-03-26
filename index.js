9e2fb654ef2bac8f34fdd387f9affa78ccc5396d
Anne Golladay <annegolladay@gmail.com>
2021-03-26 13:51:07 -0600

    Requests 1-4 (Topic 2: Advanced Array Methods) are complete

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(array){
+  function animalNames(array) {
     const displayNames = [];
     array.forEach(function(item){
       const newDisplayNames = `name: ${item.animal_name}, scientific: ${item.scientific_name}`;
@@ -75,20 +75,26 @@ const zooAnimals = [
   For example: ['jackal, asiatic', .....]
   */
 
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowerCaseNames(array) {
+    const newArray = array.map(function(item){
+      return item.animal_name.toLowerCase();
+    });
+    return newArray;
   }
-  
+  console.log('Request 2:', lowerCaseNames(zooAnimals));
   
   /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
   The zoo is concerned about animals with a lower population count. 
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowPopulationAnimals(array) {
+    const newArray = array.filter(function(item){
+      return item.population < 5;
+    });
+    return newArray;
   }
-  
+  console.log('Request 3:', lowPopulationAnimals(zooAnimals));
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
   The zoo needs to know their total animal population across the United States. 
@@ -96,9 +102,13 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
+  function USApop(array) {
+    const totalPopulation = array.reduce(function(acc, item){
+      return acc + item.population;
+    }, 0);
+    return totalPopulation;
   }
+  console.log('Request 4:', USApop(zooAnimals));
   
   
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
